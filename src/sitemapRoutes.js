cab1432005c7ae4c9b2af1d21362387865e2fcbc
Matthew Blode <m@blode.co>
2019-05-29 09:02:30 +1000

    Sitemap

---
+++ src/sitemapRoutes.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Route } from 'react-router';
+
+export default (
+    <Route>
+        <Route path='/' />
+        <Route path='/newest/1' />
+        <Route path='/show/1' />
+        <Route path='/ask/1' />
+        <Route path='/jobs/1' />
+    </Route>
+);
