a2d5d793648a28d282a4dc4335111a882d598b04
Matthew Blode <m@blode.co>
2020-03-31 21:41:54 +1100

    Fix flash of white

--- src/store.js
+++ src/store.js
@@ -6,13 +6,11 @@ import rootReducer from './reducers/rootReducer';
 
 export const history = createBrowserHistory();
 
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-
 export default function configureStore(preloadedState) {
     const store = createStore(
         rootReducer(history),
         preloadedState,
-        composeEnhancers(applyMiddleware(routerMiddleware(history), thunk))
+        compose(applyMiddleware(routerMiddleware(history), thunk))
     );
 
     return store;
